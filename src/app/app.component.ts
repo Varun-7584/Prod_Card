7b8811dca474cc3001cdd0a9020d92755130812f
Varun-7584 <115446298+Varun-7584@users.noreply.github.com>
2022-12-13 15:14:40 +0530

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,85 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  posts =[
+    {
+      title : 'Product 1 ',
+      imageUrl :'assets/P1.png',
+      Category : 'Category 1' ,
+      content :'description 1'
+    },
+    {
+      title : 'Product 2 ',
+      imageUrl :'assets/P1.png',
+      Category : 'Category 2' ,
+      content :'description 2'
+    },
+    {
+      title : 'Product 3 ',
+      imageUrl :'assets/P1.png',
+      Category : 'Category 3' ,
+      content :'description 3'
+    },
+    {
+      title : 'Product 4 ',
+      imageUrl :'assets/P1.png',
+      Category : 'Category 4' ,
+      content :'description 4'
+    },
+    {
+      title : 'Product 5 ',
+      imageUrl :'assets/P1.png',
+      Category : 'Category 5' ,
+      content :'description 5'
+    },
+    {
+      title : 'Product 6 ',
+      imageUrl :'assets/P1.png',
+      Category : 'Category 6' ,
+      content :'description 6'
+    },
+    {
+      title : 'Product 7 ',
+      imageUrl :'assets/P1.png',
+      Category : 'Category 7' ,
+      content :'description 7'
+    },
+    {
+      title : 'Product 8 ',
+      imageUrl :'assets/P1.png',
+      Category : 'Category 8' ,
+      content :'description 8'
+    },
+    {
+      title : 'Product 9 ',
+      imageUrl :'assets/P1.png',
+      Category : 'Category 9' ,
+      content :'description 9'
+    },
+    {
+      title : 'Product 10',
+      imageUrl :'assets/P1.png',
+      Category : 'Category 10' ,
+      content :'description 10'
+    },
+    {
+      title : 'Product 11 ',
+      imageUrl :'assets/P1.png',
+      Category : 'Category 11' ,
+      content :'description 11'
+    },
+    {
+      title : 'Product n + ',
+      imageUrl :'assets/P1.png',
+      Category : 'Category n' ,
+      content :'description n'
+    }
+  
+  ]
+  
+}
